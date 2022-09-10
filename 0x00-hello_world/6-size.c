bb99e308c662cd2a76958b60fa66b8251eb67ea4
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-10 10:52:23 -0700

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-/**
-* main - prints the size of various types 
- *
- * Return: Always 0 (Success)
-*/
+/* main - prints the size of various types */
+/* Return: Always 0 (Success)*/
 int main(void)
 {
 int a;
