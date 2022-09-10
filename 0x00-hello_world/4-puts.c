9cfc4156e00723b8ef514e2613bad7b2545469bd
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-10 11:55:07 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 /**
 * main -Entr point
 *
-* Description prints exactly "Programming is like building a multilingual puzzle
+* prints exactly "Programming is like building a multilingual puzzle
 *
-*Return 0
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
