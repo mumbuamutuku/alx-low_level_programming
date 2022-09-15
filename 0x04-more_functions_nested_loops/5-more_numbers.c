443e6b4b7e2927772c3358d6b0aa4a1d2cd1c1cd
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-15 13:09:01 -0700

    C

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,19 +2,20 @@
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14,
  * followed by a new line.
- * 
  * Return: numbers
  */
 void more_numbers(void)
 {
-    for (int i = 1; i <= 10; i++)
-    {
-        for (int j = 0; j <= 14; j++)
-        {
-            if (j > 9)
-                _putchar((j / 10) + '0');            
-            _putchar((j % 10) + '0');
-        }
-        _putchar('\n');
-    }
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
