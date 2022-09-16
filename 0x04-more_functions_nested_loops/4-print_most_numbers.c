b6df7e07c333a1e556da43da1f5445c21e9698da
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-16 01:12:14 -0700

    C

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,10 @@ void print_most_numbers(void)
 {
 	int i;
 
-	while (i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
 		if (i != 2 && i != 4)
 			_putchar((i % 10) + '0');
-		i++;
 	}
 	_putchar('\n');
 }
