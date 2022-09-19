1d83a8cf7c03314b3f6706361afb7bc15f536316
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-19 06:21:51 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
- * print_rev - a function that prints a string, in reverse, followed by a new line.
- * @c: string
+ * print_rev - a function that prints a string, in reverse,
+ * followed by a new line.
+ * @s: string
  * Return: reverse string
  */
 void print_rev(char *s)
 {
+	strrev(s);
 }
