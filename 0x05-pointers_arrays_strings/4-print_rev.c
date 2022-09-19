8462af6001ea0d581a8cb5848524fb206d8560af
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-19 06:34:22 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,8 +9,8 @@ void print_rev(char *s)
 {
 	int len, i;
 
-	len = strlen(s);
-	for (i < len - 1; i >= 0; i++)
+	len = strlen(s) - 1;
+	for (i < len; i >= 0; i++)
 		_putchar(s[i]);
 	_putchar('\n');
 }
