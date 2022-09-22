5ade44411d878b3da1647e1f11f5fed846aadc0a
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-22 14:38:33 +0300

    pointers

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy -function that copies a string.
+ * @dest: - string one
+ * @src: - string two
+ * @n: number of bytes
+ * Return: copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+    int i = 0;
+    if(NULL == src || NULL == dest){
+        return NULL;
+    }
+    while(src[i] != '\0')
+              i++;
+    for (i = 0; src[i] && i < n; i++)
+        dest[i] = src[i];
+
+
+    dest[i] = '\0';
+    return dest;
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h" 
+/**
+ * reverse_array -a function that reverses the content of an array of integers.
+ * @a: array 
+ * @n: is the number of elements of the array
+ * Return: reversed array
+ */
+void reverse_array(int *a, int n)
+{
+    int aux[n], i;
+
+    for (i = 0; i < n; i++)
+        aux[n-1-i] = a[i];
+    for(i = 0; i < n; i++)
+        a[i] = aux[i];
+
+}
