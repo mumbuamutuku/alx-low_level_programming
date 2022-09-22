10dffc5cde93d8f0c407a1c422d60534de232099
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-22 13:04:27 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,10 +7,12 @@
  */
 void reverse_array(int *a, int n)
 {
-	int aux[], i;
+	int aux, i;
 
-	for (i = 0; i < n; i++)
-		aux[n - 1 - i] = a[i];
-	for (i = 0; i < n; i++)
-		a[i] = aux[i];
+	for (i = n - 1; i >= n / 2; i--)
+	{
+		aux = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = aux;
+	}
 }
