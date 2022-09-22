ffa629df755c5d7c0c28ef27c8ca56c2fd5d57bd
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-22 05:14:26 -0700

    TASK 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strncat - function is similar to the _strcat function, except that 
+ * _strncat - function is similar to the _strcat function, except that
  * it will use at most n bytes from src; and -
  * src does not need to be null-terminated if it contains n or more bytes
  * @dest: - string one
@@ -10,13 +10,11 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    int len = 0, i = 0;
+	int len = 0, i = 0;
 
-    while (dest[i++])
+	while (dest[i++])
 		len++;
-
 	for (i = 0; src[i] && i < n; i++)
 		dest[len++] = src[i];
-
 	return (dest);
 }
