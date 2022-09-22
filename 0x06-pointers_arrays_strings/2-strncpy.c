df41b8d952a3d72729938938c09e9904693ecc62
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-22 05:29:07 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,16 +8,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i = 0;
-    if(NULL == src || NULL == dest){
-        return NULL;
-    }
-    while(src[i] != '\0')
-              i++;
-    for (i = 0; src[i] && i < n; i++)
-        dest[i] = src[i];
+	int i = 0;
 
-
-    dest[i] = '\0';
-    return dest;
+	if (NULL == src || NULL == dest)
+		return (NULL);
+	while (src[i] != '\0')
+		i++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
 }
