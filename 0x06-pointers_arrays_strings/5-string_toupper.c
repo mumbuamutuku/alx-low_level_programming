dd7671199e9066bd50c70d07ecc3a6c3bbf5c3d0
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-23 11:01:42 +0300

    advance task

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,21 +1,5 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
- * *string_toupper - changes all lowercase letters of a string to uppercase.
- * Return: uppercase
- */
-char *string_toupper(char *str)
- {
-        int i;
-        
-        for (i = 0; str[i] != '\0'; i++)
-        {
-                if (str[i] >= 'a' && str[i] <= 'z')
-                        str[i] = (int)str[i] - 32;
-        }
-        return (str);
-}
-=======
  * string_toupper - changes all lowercase letters of a string to uppercase.
  * @str: the string to change
  * Return: uppercase
@@ -31,4 +15,3 @@ char *string_toupper(char *str)
 	}
 	return (str);
 }
->>>>>>> 9613e562eb6a4aba53fc4a5a17f1ff707577e5ea
