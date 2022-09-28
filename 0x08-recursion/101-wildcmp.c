93c25bc76a3c900a46f0c1b3fb0af0a5a31163f5
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-28 23:34:59 +0300

    wildcard

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * wildcmp -  compares two strings 
+ * @s1: string one
+ * @s2: string two
+ * Return: 1 if the strings can be considered identical, otherwise return 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+        if (*s1 == '\0' && *s2 == '\0')
+            return (1);
+    /* Make sure to eliminate consecutive '*' */
+        if (*s1 == '*')
+        {
+            while (*(s1 + 1) == '*')
+                s1++;
+        }
+        /**
+         * Make sure that the characters after '*' are present
+         * in s2 string. This function assumes that the
+         * s1 string will not contain two consecutive '*'
+         */
+        if (*s1 == '*' && *(s1 + 1) != '\0' && *s2 == '\0')
+            return (0);
+        /**
+         * If the s1 string contains '?', or current
+         * characters of both strings wildcmp
+         */
+        if (*s1 == '?' || *s1 == *s2)
+            return (wildcmp(s1 + 1, s2 + 1));
+        /**
+         * If there is *, then there are two possibilities
+         * a) We consider current character of second string
+         * b) We ignore current character of s2 string.
+         */
+        if (*s1 == '*')
+            return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+        return (0);
+}
