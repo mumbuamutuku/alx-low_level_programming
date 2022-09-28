3a1155ae90329b5846e219228712fd308d14c080
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-28 14:29:41 -0700

    0x08. C - Recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,33 +1,34 @@
 #include "main.h"
 /**
- * match -  compares two strings 
+ * match -  compares two strings
  * @s1: string one
  * @s2: string two
+ * @after_wldcd: placeholder for position right after wildcard
  * Return: 1 if the strings can be considered identical, otherwise return 0.
  */
 int match(char *s1, char *s2, char *after_wldcd)
 {
-        if (*s1 == '\0' && *s2 == '\0')
-            return (1);
-        if (*s1 == '\0' && *s2 == '*')
-            return (match(s1, s2 + 1, s2 + 1));
-        if (*s1 == '\0' && *s2 != '\0')
-            return (0);
-        if (*s2 == '*')
-            return (match(s1, s2 + 1, s2 + 1));
-        if (*s1 == *s2)
-            return (match(s1 + 1, s2 + 1, after_wldcd));
-        else
-            return (match(s1 + 1, after_wldcd, after_wldcd));
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' && *s2 == '*')
+		return (match(s1, s2 + 1, s2 + 1));
+	if (*s1 == '\0' && *s2 != '\0')
+		return (0);
+	if (*s2 == '*')
+		return (match(s1, s2 + 1, s2 + 1));
+	if (*s1 == *s2)
+		return (match(s1 + 1, s2 + 1, after_wldcd));
+	else
+		return (match(s1 + 1, after_wldcd, after_wldcd));
 }
 int wildcmp(char *s1, char *s2)
 {
-        if (*s1 == '\0' && *s2 == '\0')
-            return (1);
-        if (*s1 == *s2)
-            return (wildcmp(s1 + 1, s2 + 1));
-        else if (*s2 == '*')
-            return (match(s1, (s2 + 1), (s2 + 1)));
-        else
-        return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s2 == '*')
+		return (match(s1, (s2 + 1), (s2 + 1)));
+	else
+		return (0);
 }
