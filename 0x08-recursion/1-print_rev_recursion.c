8d6357c0fb07a494680c9f84186fca04218d78a5
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-28 05:40:24 -0700

    0x08. C - Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,8 +6,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-	_print_rev_recursion(++s);
-	_putchar(s[0]);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
