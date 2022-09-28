d2773c5b094fcb084d773bc40ce3ca0a4da44558
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-29 00:21:20 +0300

    yes

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,37 +5,20 @@
  * @s2: string two
  * Return: 1 if the strings can be considered identical, otherwise return 0.
  */
-int match(char *s1, char *s2)
+int match(char *s1, char *s2, char *after_wldcd)
 {
         if (*s1 == '\0' && *s2 == '\0')
             return (1);
-    /* Make sure to eliminate consecutive '*' */
-        if (*s1 == '*')
-        {
-            while (*(s1 + 1) == '*')
-                s1++;
-        }
-        /**
-         * Make sure that the characters after '*' are present
-         * in s2 string. This function assumes that the
-         * s1 string will not contain two consecutive '*'
-         */
-        if (*s1 == '*' && *(s1 + 1) != '\0' && *s2 == '\0')
+        if (*s1 == '\0' && *s2 == '*')
+            return (match(s1, s2 + 1, s2 + 1));
+        if (*s1 == '\0' && *s2 != '\0')
             return (0);
-        /**
-         * If the s1 string contains '?', or current
-         * characters of both strings match
-         */
-        if (*s1 == '?' || *s1 == *s2)
-            return (match(s1 + 1, s2 + 1));
-        /**
-         * If there is *, then there are two possibilities
-         * a) We consider current character of second string
-         * b) We ignore current character of s2 string.
-         */
-        if (*s1 == '*')
-            return (match(s1 + 1, s2) || match(s1, s2 + 1));
-        return (0);
+        if (*s2 == '*')
+            return (match(s1, s2 + 1, s2 + 1));
+        if (*s1 == *s2)
+            return (match(s1 + 1, s2 + 1, after_wldcd));
+        else
+            return (match(s1 + 1, after_wldcd, after_wldcd));
 }
 int wildcmp(char *s1, char *s2)
 {
@@ -44,7 +27,7 @@ int wildcmp(char *s1, char *s2)
         if (*s1 == *s2)
             return (wildcmp(s1 + 1, s2 + 1));
         else if (*s2 == '*')
-            return (match(s1, (s2 + 1)));
+            return (match(s1, (s2 + 1), (s2 + 1)));
         else
         return (0);
 }
