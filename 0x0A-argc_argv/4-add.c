1522b97ff546e0f28379b0dc4349e5b62f939b99
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-30 12:53:48 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,23 +11,20 @@
  */
 int main(int argc, char *argv[])
 {
-    int i, j, add = 0;
-    
-    for ( i = 1; i < argc; i++)
-    {
-        for (j = 0; j < argv[i][j]; j++)
-        {
-            if (argv[i][j] < '0' || argv[i][j] > '9')
-	            {
-		printf("Error\n");
-		return (1);
-    }
-        }
-        
-        add = add + argv[i];
-    }
-    printf("%d\n", add);
+	int i, j, add = 0;
 
-    return (0);
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; j < argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add = add + atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
 }
-    
