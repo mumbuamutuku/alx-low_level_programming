805385cbcddb839373c214341c9a4dd88a519296
mumbuamutuku <mumbuamutuku@gmail.com>
2022-09-30 19:52:20 +0300

    statiic library

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strstr - function that locates a substring.
+ * @haystack: the longer string to search
+ * @needle: the substring to search for
+ * Return: a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+	int count = 0;
+
+	while (needle[count] != '\0')
+		count++;
+	while (*haystack)
+	{
+		for (index = 0; needle[index]; index++)
+		{
+			if (haystack[index] != needle[index])
+				break;
+		}
+		if (index != count)
+			haystack++;
+		else
+			return (haystack);
+	}
+	return (NULL);
+}
