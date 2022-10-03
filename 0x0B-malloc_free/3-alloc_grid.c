9552f781d102f78f8ffb9a98fa22aa04d70d374d
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-03 22:25:05 +0300

    2d arra

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,2 +1,48 @@
+#include "main.h"
+/**
+ * alloc_grid -  function that returns a pointer to a 2 dimensional array of integers.
+ * @width: width of the arrayy
+ * @height: height of the array
+ * return: NULL on failure If width or height is 0 or negative, return NULL
+ */
+
 int **alloc_grid(int width, int height)
-{}
+{
+    int i, j;
+
+    int **grid;
+
+    if (width <=0 || height <= 0)
+        return (NULL);
+    grid = malloc (height * sizeof(grid));
+    if (grid == NULL)
+    {
+        free(grid);
+        return (NULL);
+    }
+    while (i < height)
+    {
+        grid[i] = malloc(width * sizeof(**grid));
+        if (grid[i] == NULL)
+        {
+            i--;
+            while (i >= 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
+			return (NULL);
+		}
+		j = 0;
+		while (j < width)
+		{
+			grid[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+	i = 0;
+	return (grid);
+        
+}
