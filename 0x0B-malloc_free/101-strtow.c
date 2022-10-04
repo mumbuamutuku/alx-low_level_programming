ce1dab857e3d7d04c1fcec7471180b2a7466c597
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-04 21:44:37 +0300

    advanced

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,7 +6,39 @@
  */
 char **strtow(char *str)
 {
+	int len = 0, i;
+	char *p;
+	char del[] = "";
+	int index = 0, totalwords = 0;
+	int *wordindex = 0;
+
 	if (str == NULL || str == "")
 		return (NULL);
-
+	
+	while (str[len] != '\0')
+		len++;
+	
+	p = malloc(sizeof(char) * (len + 1));
+	
+	for(index = 0; index <= len; index++)
+	{
+		if (str[index] == ' ' || str[index] == '\0')
+		{
+			p[totalwords][wordindex] = '\0';
+			totalwords++;
+			wordindex = 0;
+		}
+		else
+		{
+			p[totalwords][wordindex] = str[index];
+			wordindex++;
+		}
+		
+	}
+	for (index = 0; index < totalwords; i++)
+	{
+		return (p[index]);
+	}
+	return (0);
+	
 }
