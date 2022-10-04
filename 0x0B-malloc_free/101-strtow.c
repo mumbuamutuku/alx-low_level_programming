870096af4af24ba36b5aa887be97c07024d4fae9
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-04 03:42:14 -0700

    0x0B. C - malloc, free

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * strtow - unction returns a pointer to an array of strings (words
+ * @str: string;
+ * Return: NULL if str == NULL or str == ""
+ */
+char **strtow(char *str)
+{
+	if (str == NULL || str == "")
+		return (NULL);
+
+}
