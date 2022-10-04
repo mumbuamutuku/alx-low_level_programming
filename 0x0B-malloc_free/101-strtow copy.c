75cc3ba0cf45d53a14fcda1f9adafb7b7c2f01a7
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-04 22:41:47 +0300

    advanced

---
+++ 0x0B-malloc_free/101-strtow copy.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * strtow - unction returns a pointer to an array of strings (words
+ * @str: string;
+ * Return: NULL if str == NULL or str == ""
+ */
+char **strtow(char *str)
+{
+	int len = 0, i;
+	char *p;
+	char del[] = "";
+	int index = 0, totalwords = 0;
+	int *wordindex = 0;
+
+	if (str == NULL || str == del)
+		return (NULL);
+	
+	while (str[len] != '\0')
+		len++;
+	
+	p = malloc(sizeof(char) * (len + 1));
+	
+	for(index = 0; index <= len; index++)
+	{
+		if (str[index] == ' ' || str[index] == '\0')
+		{
+			p[totalwords][wordindex] = '\0';
+			totalwords++;
+			wordindex = 0;
+		}
+		else
+		{
+			p[totalwords][wordindex] = str[index];
+			wordindex++;
+		}
+		
+	}
+	for (index = 0; index < totalwords; i++)
+	{
+		printf("%d\n", p[index]);
+	}
+	return (0);
+	
+}
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * strtow - unction returns a pointer to an array of strings (words
  * @str: string;
@@ -6,39 +8,52 @@
  */
 char **strtow(char *str)
 {
-	int len = 0, i;
-	char *p;
-	char del[] = "";
-	int index = 0, totalwords = 0;
-	int *wordindex = 0;
+	int len = 0, index = 0;
 
-	if (str == NULL || str == del)
-		return (NULL);
-	
-	while (str[len] != '\0')
-		len++;
-	
-	p = malloc(sizeof(char) * (len + 1));
-	
-	for(index = 0; index <= len; index++)
-	{
-		if (str[index] == ' ' || str[index] == '\0')
-		{
-			p[totalwords][wordindex] = '\0';
-			totalwords++;
-			wordindex = 0;
-		}
-		else
-		{
-			p[totalwords][wordindex] = str[index];
-			wordindex++;
-		}
-		
-	}
-	for (index = 0; index < totalwords; i++)
-	{
-		printf("%d\n", p[index]);
-	}
-	return (0);
-	
+	while (*(str + index) && *(str + index) != ' ')
+    {
+        len++;
+		index++;
+    }
+
+    int j = 0, words = 0, wordlen = 0;
+    for (j = 0; *(str + index); j++)
+        wordlen++;
+    for (j = 0; j < wordlen; j++)
+    {
+        if(*(str + j) != ' ')
+           {
+               words++;
+               j += len + 1;
+           }
+    }
+	char **p;
+	int w, m = 0, l;
+
+	if (str == NULL || str[0] == '\0')
+            return (NULL);
+    if (words == 0)
+        return (NULL);
+
+	p = malloc(sizeof(char) * (words + 1));
+	if (p == NULL)
+        return (NULL);
+    for(w = 0; w < words; w++)
+    {
+        while (str[m] == ' ')
+            m++;
+        p[w] = malloc(sizeof(char) * (len + 1));
+        if (p[w] == NULL)
+        {
+            for(; w >= 0; w--)
+                free(p[w]);
+            free(p);
+            return (NULL);
+        }
+        for(l = 0; l < words; l++)
+            p[w][l] = str[m++];
+        p[w][l] = '\0';
+    }
+    p[w] = NULL;
+    return (p);
 }
