6d1b666bd641d4bf734c8fbab9636c36cc664f9c
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-11 05:23:16 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+*struct dog - a struct to show elements
+*@name: name of the dog
+*@age: age of the dog
+*@owner: owner of the dog
+*/
 struct dog
 {
 	char *name;
