9c706f7a3504ae4ff95fb7d6ca19886268fae373
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-13 13:59:51 +0300

    0x0F. C - Function pointers C

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name- a function that prints a name.
+ * @f: pointer to vod function
+ *  @name: Name to print
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    f(name);
+}
