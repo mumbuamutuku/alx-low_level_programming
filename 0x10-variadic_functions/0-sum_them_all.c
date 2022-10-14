8386382492cbe40ca598d22f86e2fa8177bfa757
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-14 06:38:12 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,10 +7,9 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i, sum = 0;
+	unsigned int i, sum;
 
-	if (n == 0)
-		return (0);
+	i = sum = 0;
 	va_start(ap, n);
 	/**
 	*for (i = 0; i < n; i++)
