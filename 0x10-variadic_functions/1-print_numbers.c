caf23b51c1a00167b793c6761d49fdae47833df6
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-14 03:18:50 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,11 +7,16 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    unsigned int index;
-    va_list nums;
+	unsigned int index;
+	va_list nums;
 
-    va_start(nums, n);
-    for (index = 0; index < n; index++)
-        printf("%d%c", index, separator);
-    va_end(nums);
+	va_start(nums, n);
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(nums, int));
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(nums);
 }
