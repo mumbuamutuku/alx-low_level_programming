417c55cc2c5091835a6edd8f7bf5108c1ddd8da2
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-24 23:28:02 +0300

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,17 @@
-global    _start          
-section   .text_start:   
-mov       rax, 1                  ; system call for write         
-mov       rdi, 1                  ; file handle 1 is stdout         
-mov       rsi, message            ; address of string to output          
-mov       rdx, 13                 ; number of bytes          
-syscall                           ; invoke operating system to do the write         
-mov       rax, 60                 ; system call for exit          
-xor       rdi, rdi                ; exit code 0          
-syscall                           ; invoke operating system to exit          
+section .data
+	fmt db	"%s", 10, 0
+	msg db	"Hello, Holberton", 0
 
-section   .datamessage 
-	message: db        "Hello, Holberton", 10      ; note the newline at the end
+	section .text
+	extern printf
+	global main
+
+main:
+	push	rbp
+	mov	rsi, msg
+	mov	rdi, fmt
+	mov	rax, 0
+	call 	printf
+	pop	rbp
+	mov	rax, 0
+	ret
