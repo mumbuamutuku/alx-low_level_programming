c7d12ed406b2f040a4cc2d42e41285c9404cb81a
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-27 11:54:03 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -22,7 +22,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 	newnode = *head;
 	while (newnode->next != NULL)
-	 	newnode = newnode->next;
+		newnode = newnode->next;
 	newnode->next = new;
 	return (*head);
 }
