b7956666346d504b08bfe0f78e411d707df7ef63
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-31 14:20:58 +0300

    0x14. C - Bit manipulation

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+}
