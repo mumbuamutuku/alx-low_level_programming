1f867fe0fc190d425007df395cef721b1c64b6e1
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-31 07:37:56 -0700

    0x14. C - Bit manipulation C

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,13 +5,12 @@
  * @n: The number provided.
  * Return: 1 if it worked, or -1 if an error occurred
 */
-
 int set_bit(unsigned long int *n, unsigned int index)
 {
-        if (index > 63)
-	    {
-		    return (-1);
-	    }
-        *n ^= (1 << index);
-        return (1);
+	if (index > 63)
+	{
+		return (-1);
+	}
+	*n ^= (1 << index);
+	return (1);
 }
