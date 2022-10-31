56c2bfe7af645e966209d1888b16faeb0d02f5cd
mumbuamutuku <mumbuamutuku@gmail.com>
2022-10-31 08:01:33 -0700

    0x14. C - Bit manipulation C

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,16 +4,16 @@
  *             flipped to get from one number to another.
  * @n: The number.
  * @m: The number to flip n to.
+*Return: number o bits
 */
-
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int flip = n ^ m, b = 0;
+	unsigned long int flip = n ^ m, b = 0;
 
-    while (flip > 0)
-    {
-        b += (flip & 1);
-        flip >>= 1;
-    }
-    return (b);
+	while (flip > 0)
+	{
+		b += (flip & 1);
+		flip >>= 1;
+	}
+	return (b);
 }
