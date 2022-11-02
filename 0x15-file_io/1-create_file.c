7851ba8e85f93cd475bbe189ca7a7ea03cca3a6f
mumbuamutuku <mumbuamutuku@gmail.com>
2022-11-02 11:41:36 -0700

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,19 +8,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int op, wr, len = 0;
+	int op, wr, len = 0;
 
-    if (filename == NULL)
-        return (-1);
-    if (text_content != NULL)
-    {
-        for (len = 0; text_content[len];)
-            len++;      
-    }
-    op = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
-    wr = write(op, text_content, len);
-    if (op == -1 || wr == -1)
-        return (-1);
-    close(op);
-    return (1);
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	op = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
+	wr = write(op, text_content, len);
+	if (op == -1 || wr == -1)
+		return (-1);
+	close(op);
+	return (1);
 }
