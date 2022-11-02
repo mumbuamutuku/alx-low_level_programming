7621f36e2915a61337ac35a8629eddf3900749cb
mumbuamutuku <mumbuamutuku@gmail.com>
2022-11-02 21:32:57 +0300

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
     if (text_content != NULL)
     {
         for (len = 0; text_content[len];)
-            len++       
+            len++;      
     }
     op = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
     wr = write(op, text_content, len);
@@ -23,6 +23,4 @@ int create_file(const char *filename, char *text_content)
         return (-1);
     close(op);
     return (1);
-}
-    
 }
