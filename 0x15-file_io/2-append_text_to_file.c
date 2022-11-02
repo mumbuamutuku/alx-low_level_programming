32660bd2c96d9d7f98678f496c6347544f3c3400
mumbuamutuku <mumbuamutuku@gmail.com>
2022-11-02 12:37:43 -0700

    0x15. C - File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,20 +7,21 @@
   */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int op, wr, len = 0;
-   if (filename == NULL)
+	int op, wr, len = 0;
+
+	if (filename == NULL)
 		return (-1);
-op = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
+	op = open(filename, O_WRONLY | O_APPEND);
 	if (op == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
 		while (text_content[len] != '\0')
 			len++;
-	    wr = write(op, text_content, len);
-	    if (wr == -1)
-		    return (-1);
-    }
+		wr = write(op, text_content, len);
+		if (wr == -1)
+			return (-1);
+	}
 	close(op);
 	return (1);
 }
