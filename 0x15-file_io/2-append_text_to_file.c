e4b8dfd609d72b50624d72a0a6d36a642194504a
mumbuamutuku <mumbuamutuku@gmail.com>
2022-11-02 22:01:14 +0300

    0x15. C - File I/O

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * append_text_to_file - Open a file and add text to the end of the file
+  * @filename: File to open and append to
+  * @text_content: Text to append to file
+  * Return: 1 on success, -1 on failures
+  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+    int op, wr, len = 0;
+   if (filename == NULL)
+		return (-1);
+op = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
+	if (op == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
+	    wr = write(op, text_content, len);
+	    if (wr == -1)
+		    return (-1);
+    }
+	close(op);
+	return (1);
+}
