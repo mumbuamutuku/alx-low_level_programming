9993f6d3450c0abd3484ebf03083ede6be1bc13e
mumbuamutuku <mumbuamutuku@gmail.com>
2023-01-08 11:06:53 -0800

    qn 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,29 +4,29 @@
  * hash_table_set - a function that adds an element to the hash table.
  * @ht: is the hash table you want to add or update the key/value to
  * @key: is the key. key can not be an empty string
- * @value: is the value associated with the key. value must be duplicated. 
+ * @value: is the value associated with the key. value must be duplicated.
  * value can be an empty string
  * Return: 1 if it succeeded, 0 otherwise
  * In case of collision, add the new node at the beginning of the list
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int i;
-    char *str;
-    hash_node_t *node;
+	unsigned long int i;
+	char *str;
+	hash_node_t *node;
 
-    if (!ht || !key)
-        return (0);
-    
-    i = key_index((unsigned char *)key, ht->size);
-    if (value)
-        str = strdup(value);
-    else
-        str = NULL;
-    node = ht->array[i];
+	if (!ht || !key)
+		return (0);
 
-    if (!node)
-    node = new_node(NULL);
+	i = key_index((unsigned char *)key, ht->size);
+	if (value)
+		str = strdup(value);
+	else
+		str = NULL;
+	node = ht->array[i];
+
+	if (!node)
+		node = new_node(NULL);
 
 	if (node->key)
 	{
@@ -39,5 +39,4 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	ht->array[i] = node;
 	return (1);
-   
 }
